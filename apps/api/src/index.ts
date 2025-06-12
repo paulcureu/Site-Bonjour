b1fef40395c4ea1f600b50f021e3ea8ee129d68e
Paul <cureu.paul.ioan@gmail.com>
2025-06-12 18:25:37 +0300

    fix: cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,9 @@
+// apps/api/src/index.ts
+
 import express from 'express';
 import authRoutes from './routes/auth';
 import statusRoutes from './routes/status';
 import adminRoutes from './routes/dashboard';
-// ­–––– DEBUG: afișează toate rutele încărcate ––––
 import { env } from './env';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './utils/swagger';
@@ -19,12 +20,11 @@ const corsOptions = {
   origin: 'http://localhost:5173',
   optionsSuccessStatus: 200,
 };
-// 🟢 trebuie să fie primul!
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
-//ruta cors
-app.use(cors(corsOptions));
-// 🟢 rute API
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/status', statusRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
@@ -32,10 +32,10 @@ app.use('/api/v1/admin', adminRoutes);
 app.use('/api/v1/reservations', reservationRoutes);
 app.use('/api/v1/reviews', reviewRoutes);
 app.use('/api/v1/menu-items', menuItemRoutes);
-
 app.use('/api/v1', debugRedis);
 
 mountBullBoard(app);
+
 const port = Number(env.PORT);
 const isProduction = process.env.NODE_ENV === 'production';
 const host = isProduction ? '0.0.0.0' : 'localhost';
